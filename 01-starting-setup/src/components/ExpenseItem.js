901486513c4d969827d311a7205f2117a3c78322
jieunheo <gjekdmlwl@naver.com>
2022-04-18 12:54:18 +0900

    className 오타 수정

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -8,7 +8,7 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <div>{props.date.toISOString()}</div>
-      <div className="expense-item__item__description">
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
