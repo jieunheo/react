6faff49ff067d05476787d64f159d0ee21fbd491
jieunheo <gjekdmlwl@naver.com>
2022-04-18 14:59:33 +0900

    state를 이용하여 title 수정해보기

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
@@ -6,13 +6,22 @@ import Card from "../UI/Card";
 import './ExpenseItem.css';
 
 const ExpenseItem = props => {
-  const clickHandler = () => {console.log('click!!')}
+  //React Hook이라고 불림
+  //Component 안에서 호출 되어야 함
+  //첫전째 값: 변수 자체인 배열 반환
+  //두번째 값: 업데이트되는 함수
+  //=> 배열구조분해 사용
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Update!');
+  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
