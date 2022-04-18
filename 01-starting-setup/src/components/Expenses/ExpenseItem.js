c92433e698c19b395dfa75cd3c9c16b8edc48903
jieunheo <gjekdmlwl@naver.com>
2022-04-18 15:22:27 +0900

    오타 수정

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -8,7 +8,7 @@ import './ExpenseItem.css';
 const ExpenseItem = props => {
   //React Hook이라고 불림
   //Component 안에서 호출 되어야 함
-  //첫전째 값: 변수 자체인 배열 반환
+  //첫번째 값: 변수 자체인 배열 반환
   //두번째 값: 업데이트되는 함수
   //=> 배열구조분해 사용
   const [title, setTitle] = useState(props.title);
