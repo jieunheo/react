c7d795602c9aa5f63531e85ce3db95d63e0d8709
jieunheo <gjekdmlwl@naver.com>
2022-04-19 12:12:03 +0900

    폼 전송 시 입력된 값을 객체로 받기

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -62,8 +62,19 @@ const ExpenseForm = () => {
     console.log(event.target.value);
   };
 
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+    console.log(enteredData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
