2876b72fc06d39b0700db53fdbd9fc5dbde1507c
jieunheo <gjekdmlwl@naver.com>
2022-04-20 12:53:18 +0900

    폼 나타내기 숨기기 버튼 추가 및 기능 적용

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = props => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = enteredExpenseData => {
     const expanseData = {
       ...enteredExpenseData,
@@ -11,11 +13,27 @@ const NewExpense = props => {
     }
     console.log('NewExpanse.js - expanseData: ', expanseData);
     props.onAddExpenseHaldler(expanseData);
+    setIsEditing(false);
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Ass New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler} />
+      )}
     </div>
   );
 }
