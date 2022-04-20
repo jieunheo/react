ce6575ea84fce246d5218234ccc75c24bed63a5f
jieunheo <gjekdmlwl@naver.com>
2022-04-20 11:45:21 +0900

    이전 리스트 추가 값이 이상한 이유 발견 - 자체 관리하고 있던 state값 삭제

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React/*, { useState }*/ from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
@@ -11,20 +11,20 @@ const ExpenseItem = props => {
   //첫번째 값: 변수 자체인 배열 반환
   //두번째 값: 업데이트되는 함수
   //=> 배열구조분해 사용
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle('Update!');
-  }
+  // const clickHandler = () => {
+  //   setTitle('Update!');
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
