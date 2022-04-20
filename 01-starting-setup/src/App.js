bf768ceb9b33226aeee7271b8b23542da304095b
jieunheo <gjekdmlwl@naver.com>
2022-04-20 11:42:01 +0900

    폼에 입력한 값을 state를 이용하여 리스트에 추가

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,47 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12)
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  // {
+  //   id: 'e4',
+  //   title: 'New Desk (Wooden)',
+  //   amount: 450,
+  //   date: new Date(2021, 5, 12),
+  // },
+];
 
-  const addExpanseHandler = expense => {
-    const expenseData = {
-      ...expense
-    }
-    console.log('App.js - expenseData: ', expenseData);
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = expense => {
+    console.log(expense.title, expense.amount, expense.date);
+    setExpenses(preExpenses => {
+      return [expense, ...preExpenses];
+    });
   };
 
   return (
     <div>
       <h2>시작</h2>
-      <NewExpense onAddExpenseHaldler={addExpanseHandler} />
+      <NewExpense onAddExpenseHaldler={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
