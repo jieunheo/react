e7f5fe961edea7ba80c3a5c7a44c1e5df3f05605
jieunheo <gjekdmlwl@naver.com>
2022-04-21 10:18:22 +0900

    styled componentes 빼고 원래 상태로 되돌리기

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const FormCntrol = styled.div`
-  margin: 0.5rem 0;
+// styled-components를 이용한 동적 스타일 입히기
+// const FormCntrol = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    color: ${props => props.invalid ? 'red' : 'black'};
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-  }
+//   & label {
+//     color: ${props => props.invalid ? 'red' : 'black'};
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
-    background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+//     background: ${props => props.invalid ? '#ffd7d7' : 'transparent'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -53,10 +54,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormCntrol invalid={!isValid}>
+      <div className={`form-control ${!isValid && 'invalid'}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormCntrol>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
