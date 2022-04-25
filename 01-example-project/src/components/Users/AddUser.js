9bcbb2e8e9b39cd0dd9dad2f581f12f40392eba0
jieunheo <gjekdmlwl@naver.com>
2022-04-25 10:20:09 +0900

    유효성 검사 및 전송시 폼 리셋 기능 추가

--- 01-example-project/src/components/Users/AddUser.js
+++ 01-example-project/src/components/Users/AddUser.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = props => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = event => {
     event.preventDefault();
+
+    // 값이 하나라도 비어있다면
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    // 나이가 0 이하라면
+    if(+enteredAge <= 0) {
+      return;
+    }
+
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
+
+  const usernameChangeHandler = event => {
+    setEnteredUsername(event.target.value);
+  }
+
+  const ageChangeHandler = event => {
+    setEnteredAge(event.target.value);
+  }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input id='username' type='text' value={enteredUsername} onChange={usernameChangeHandler} />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' />
+        <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler} />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
