022adfcbe623a95c2279d2d9479205d000b18d7c
jieunheo <gjekdmlwl@naver.com>
2022-04-27 16:25:15 +0900

    password에서 useReducer 사용

--- 02-starting-project/src/components/Login/Login.js
+++ 02-starting-project/src/components/Login/Login.js
@@ -20,20 +20,45 @@ const emailReducer = (state, action) => {
 
   return {
     value: "",
-    isValid: false,
+    isValid: false
   };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === 'PASSWORD_INPUT') {
+    return {
+      value: action.val,
+      isValid: action.val.trim().length > 6
+    }
+  }
+  if (action.type === 'PASSWORD_BLUR') {
+    return {
+      value: state.value,
+      isValid: state.value.trim().length > 6
+    }
+  }
+
+  return {
+    value: '',
+    isValid: false
+  }
 };
 
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispathEmail] = useReducer(emailReducer, {
     value: "",
-    isValid: false,
+    isValid: undefined
+  });
+
+  const [passwordState, dispathPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: undefined
   });
 
   useEffect(() => {
@@ -62,12 +87,12 @@ const Login = (props) => {
     dispathEmail({type: 'USER_INPUT', val: event.target.value});
 
     setFormIsValid(
-      event.target.value.includes("@") && enteredPassword.trim().length > 6
+      event.target.value.includes("@") && passwordState.value.trim().length > 6
     );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispathPassword({type: 'PASSWORD_INPUT', val: event.target.value});
 
     setFormIsValid(
       emailState.isValid && event.target.value.trim().length > 6
@@ -79,12 +104,12 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispathPassword({type: 'PASSWORD_BLUR'});
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -106,14 +131,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
