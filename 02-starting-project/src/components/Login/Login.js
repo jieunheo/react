4808620ed3fb9aeeeca24567db063cd48993c721
jieunheo <gjekdmlwl@naver.com>
2022-05-05 15:06:28 +0900

    useContext를 이용하여 provider에서 로그인 기능을 독립적으로 사용하도록 하기

--- 02-starting-project/src/components/Login/Login.js
+++ 02-starting-project/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -61,6 +62,9 @@ const Login = (props) => {
     isValid: null
   });
 
+  // auth context
+  const ctx = useContext(AuthContext);
+
   useEffect(() => {
     console.log("effect running");
 
@@ -115,7 +119,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
