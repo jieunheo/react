6c0e8c753ff1ae62d667e0acb70380d8aca9bcf9
heojieun <gjekdmlwl@naver.com>
2022-05-11 10:58:24 +0900

    컨텍스트를 이용하여 장바구니에 담긴 아이템의 amount 값 가져오기

--- 03-practice-project/src/components/Layout/HeaderCartButton.js
+++ 03-practice-project/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+  const cartContext = useContext(CartContext);
+
+  // reduce(함수, 초기값);
+  //         |-> (현재값, 살펴보고있는항목) => {}
+  const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return(
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
