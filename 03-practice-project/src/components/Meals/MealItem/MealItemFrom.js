95e5f1186a35c9c6b7d7b0f688ba06ea3e5dce33
heojieun <gjekdmlwl@naver.com>
2022-05-11 10:33:56 +0900

    input 컴포넌트 안에서 id 값이 중복되는 현상 수정

--- 03-practice-project/src/components/Meals/MealItem/MealItemFrom.js
+++ 03-practice-project/src/components/Meals/MealItem/MealItemFrom.js
@@ -6,14 +6,17 @@ import classes from './MealItemFrom.module.css';
 const MealItemFrom = props => {
   return (
     <from className={classes.form}>
-      <Input label='Amount' input={{
-        id: 'amount',
-        type: 'number',
-        min: '1',
-        max: '5',
-        step: '1',
-        defaultValue: '1'
-      }} />
+      <Input
+        label='Amount'
+        input={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1'
+        }}
+      />
       <button>+ Add</button>
     </from>
   );
