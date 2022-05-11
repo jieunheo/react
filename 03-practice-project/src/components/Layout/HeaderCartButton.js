658587d1200c46ff61964ee30f0fbc07f6911a2b
heojieun <gjekdmlwl@naver.com>
2022-05-11 14:40:48 +0900

    버튼 애니메이션 효과(카트 amount 값이 수정되는 경우 애니메니션 실행)

--- 03-practice-project/src/components/Layout/HeaderCartButton.js
+++ 03-practice-project/src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
+  
+  const { items } = cartContext;
 
   // reduce(함수, 초기값);
   //         |-> (현재값, 살펴보고있는항목) => {}
-  const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if(items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      // 애니메이션 시간 후 false로 수정
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      // cleanup 함수 생성
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return(
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
