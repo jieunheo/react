351a3544a38b45ca3f0b85fdb1b2ceb69255062a
heojieun <gjekdmlwl@naver.com>
2022-05-11 13:37:08 +0900

    cart에 추가하는 값이 같은 값인 경우 해당 갯수를 증가하여 하나의 아이템으로 보여주기

--- 03-practice-project/src/store/CartProvider.js
+++ 03-practice-project/src/store/CartProvider.js
@@ -13,8 +13,25 @@ const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
     // concat: 배열에 새 항목 추가
     //         -> 배열을 직접 수정하는 것이 아니라 새 배열을 반환함
-    const updatedItems = state.items.concat(action.item);
     const updateTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      // 추가한 값의 id와 기존의 값의 id를 비교
+      item => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if(existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      }
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
 
     return {
       items: updatedItems,
