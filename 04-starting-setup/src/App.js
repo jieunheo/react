af1734b4b0e2c27cf573f70a3aa71205511ec3b4
heojieun <gjekdmlwl@naver.com>
2022-05-12 11:32:27 +0900

    토글 버튼 추가

--- 04-starting-setup/src/App.js
+++ 04-starting-setup/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING!');
+
+  const toggleParagraphHaldler = () => {
+    setShowParagraph(prevParagraph => !prevParagraph);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHaldler}>Show Paragraph!</Button>
     </div>
   );
 }
