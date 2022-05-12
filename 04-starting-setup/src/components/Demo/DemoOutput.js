9669b5aec7973f2e57635b2d5fe9806b923b9a16
heojieun <gjekdmlwl@naver.com>
2022-05-12 13:13:37 +0900

    React.memo를 적용 시 해당 컴포넌트의 props 수정 여부로 컴포넌트 재평가

--- 04-starting-setup/src/components/Demo/DemoOutput.js
+++ 04-starting-setup/src/components/Demo/DemoOutput.js
@@ -7,4 +7,7 @@ const DemoOutput = props => {
   return <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>;
 };
 
-export default DemoOutput;
+// React.memo: 인자로 입력되는 컴포넌트의 모든 props 신규 값을 확인한 뒤
+//             기존 props 값과 비교하도록 리액트에 전달
+//             -> props가 수정된 경우에만 컴포넌트를 재실행 및 재평가
+export default React.memo(DemoOutput);
