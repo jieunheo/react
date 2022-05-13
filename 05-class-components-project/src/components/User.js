c6db09c56a72fd89ffdabf5dc3c705a5f93b4fc2
heojieun <gjekdmlwl@naver.com>
2022-05-13 14:25:07 +0900

    componentWillUnmount을 이용하여 컴포넌트가 사질 때 호출

--- 05-class-components-project/src/components/User.js
+++ 05-class-components-project/src/components/User.js
@@ -11,6 +11,11 @@ class User extends Component {
   }
   */
 
+  // 컴포넌트가 사라질 때 호출
+  componentWillUnmount() {
+    console.log('User will unmount!');
+  };
+
   // 함수형 컴포넌트의 반환 문장과 동일
   // Component를 확장했기 때문에 this를 이용하여 props를 가져올 수 있음
   render() {
