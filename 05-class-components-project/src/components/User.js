d3e16988b46ee2b91b34e099d36516ba40c0f62b
heojieun <gjekdmlwl@naver.com>
2022-05-13 11:20:06 +0900

    클래스 기반 컴포넌트로 만들기

--- 05-class-components-project/src/components/User.js
+++ 05-class-components-project/src/components/User.js
@@ -1,7 +1,25 @@
+import React, { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// extends: 확장
+class User extends Component {
+  /*
+  // 생성자 메소드
+  constructor() {
+    // 초기와 작업
+  }
+  */
+
+  // 함수형 컴포넌트의 반환 문장과 동일
+  // Component를 확장했기 때문에 this를 이용하여 props를 가져올 수 있음
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  };
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
