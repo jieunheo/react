f8ed6e45703c1cb7bae1c49f343dd6da7e0f8118
heojieun <gjekdmlwl@naver.com>
2022-05-13 09:39:46 +0900

    Demo list 컴포넌트 생성 및 적용

--- 04-starting-setup/src/App.js
+++ 04-starting-setup/src/App.js
@@ -1,32 +1,21 @@
 import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
+import DemoList from './components/Demo/DemoList';
 import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
-
-  console.log('APP RUNNING!');
-
+  const [listTitle, setLisrTitle] = useState('My List');
+  
   // useCallback: 가상의 공간에 해당 값을 저장한 후 그 값이 수정되는지 확인
-  const toggleParagraphHaldler = useCallback(() => {
-    if(allowToggle) {
-      setShowParagraph(prevParagraph => !prevParagraph);
-    }
-  }, [allowToggle]);
-
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  }
+  const changeTitleHaldler = useCallback(() => {
+    setLisrTitle('New Title');
+  }, []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
-      <Button onClick={toggleParagraphHaldler}>Show Paragraph!</Button>
+      <DemoList title={listTitle} items={[5, 3, 1, 10, 9]}/>
+      <Button onClick={changeTitleHaldler}>Change List Title!</Button>
     </div>
   );
 }
