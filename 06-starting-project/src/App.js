fe3f6e544377906e6aa7edb5927bdf7d7b1be4fb
heojieun <gjekdmlwl@naver.com>
2022-05-16 17:30:38 +0900

    오류 상황인 경우 오류 문구 출력하기

--- 06-starting-project/src/App.js
+++ 06-starting-project/src/App.js
@@ -11,26 +11,40 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   // then() 을 대신하는 문법
   // 프로미스를 사용하는 함수에는 async
   // 프로미스를 반환하는 작업 앞에는 await를 쓴다
   async function fetchMovieHandler () {
-    setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json();
-    
-    const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.relese_date,
-        openingText: movieData.opening_crawl
-      }
-    });
-    setMovies(transformedMovies);
+    try {
+      setIsLoading(true);
+      setError(null); // 이전에 받았을 수도 있는 오류를 초기화
+      const response = await fetch('https://swapi.dev/api/films/')
+
+      // 바디부분을 파싱하기 전에 error 캐치
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      };
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.relese_date,
+          openingText: movieData.opening_crawl
+        }
+      });
+
+      setMovies(transformedMovies);
+    } catch(error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
+  
 
   return (
     <React.Fragment>
@@ -40,7 +54,8 @@ function App() {
       <section>
         {/* {isLoading ? <p>Loading...</p> : <MoviesList movies={movies} />} */}
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length == 0 && <p>Found no movies.</p>}
+        {!isLoading && movies.length == 0 && !error && <p>Found no movies.</p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
