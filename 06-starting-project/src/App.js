12ad5bc90e9d3fee4adc229739ab205cce0ca283
heojieun <gjekdmlwl@naver.com>
2022-05-16 17:04:10 +0900

    then() 을 대신 async, await를 사용하여 비동기 구현

--- 06-starting-project/src/App.js
+++ 06-starting-project/src/App.js
@@ -11,22 +11,22 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMovieHandler () {
-    fetch('https://swapi.dev/api/films/')
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          releaseDate: movieData.relese_date,
-          openingText: movieData.opening_crawl
-        }
-      });
-      setMovies(transformedMovies);
+  // then() 을 대신하는 문법
+  // 프로미스를 사용하는 함수에는 async
+  // 프로미스를 반환하는 작업 앞에는 await를 쓴다
+  async function fetchMovieHandler () {
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data = await response.json();
+    
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        releaseDate: movieData.relese_date,
+        openingText: movieData.opening_crawl
+      }
     });
+    setMovies(transformedMovies);
   };
 
   return (
