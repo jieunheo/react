2099b8b1e3e4b3d7742adde90583ee4b2dfc1376
heojieun <gjekdmlwl@naver.com>
2022-05-17 11:29:25 +0900

    커스텀 훅 만들기

---
+++ 07-custom-hook-project/src/hooks/use-counter.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+// useXXX
+// 리액트에게 해당 함수를 커스텀 훅 규칙에 따라 사용하겠다고 알림
+const useCounter = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+};
+
+export default useCounter;
