c90784ee135bba5e495dcef54d4ae22b9cde0cf5
heojieun <gjekdmlwl@naver.com>
2022-05-17 12:37:53 +0900

    useCounter 훅 확장

--- 07-custom-hook-project/src/hooks/use-counter.js
+++ 07-custom-hook-project/src/hooks/use-counter.js
@@ -2,16 +2,20 @@ import { useState, useEffect } from 'react';
 
 // useXXX
 // 리액트에게 해당 함수를 커스텀 훅 규칙에 따라 사용하겠다고 알림
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if(forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 };
