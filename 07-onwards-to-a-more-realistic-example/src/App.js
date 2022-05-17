f84011a63e1dcce65121169a6c38a3f56d00044f
heojieun <gjekdmlwl@naver.com>
2022-05-17 14:44:15 +0900

    app 컴포넌트에서 useCallback 없이 구현하기

--- 07-onwards-to-a-more-realistic-example/src/App.js
+++ 07-onwards-to-a-more-realistic-example/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -7,18 +7,8 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = useCallback(taskObj => {
-    const loadedTasks = [];
-
-      for (const taskKey in taskObj) {
-        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-  }, []);
-
   // :별칭
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   // const fetchTasks = async (taskText) => {
   //   setIsLoading(true);
@@ -48,8 +38,18 @@ function App() {
   // };
 
   useEffect(() => {
-    fetchTasks({ url: 'https://react-study-2cfc9-default-rtdb.firebaseio.com/tasks.json' });
-  }, [fetchTasks]);
+    const transformTasks = taskObj => {
+      const loadedTasks = [];
+  
+        for (const taskKey in taskObj) {
+          loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+        }
+  
+        setTasks(loadedTasks);
+    };
+
+    fetchTasks({ url: 'https://react-study-2cfc9-default-rtdb.firebaseio.com/tasks.json' }, transformTasks);
+  }, []);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
