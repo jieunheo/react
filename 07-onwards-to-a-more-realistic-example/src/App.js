db2bfb2f1475e015bedd9509f5f9eca69e76d6c1
heojieun <gjekdmlwl@naver.com>
2022-05-17 13:01:52 +0900

    내 firebase 연결

--- 07-onwards-to-a-more-realistic-example/src/App.js
+++ 07-onwards-to-a-more-realistic-example/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://react-study-2cfc9-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
