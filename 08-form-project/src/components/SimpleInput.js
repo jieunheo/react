ca077c7f46d4da38e5f11da6e6e1f1f1602b6d11
heojieun <gjekdmlwl@naver.com>
2022-05-20 10:05:15 +0900

    ref를 이용하여 input 값 가져오기 및 submit 시 초기화

--- 08-form-project/src/components/SimpleInput.js
+++ 08-form-project/src/components/SimpleInput.js
@@ -1,9 +1,30 @@
+import React, { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  }
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    console.log(enteredName); // 상태값
+    // current: ref에 할당한 값을 가지고 있음(여기서는 input을 포인터)
+    //        .value: 해당 포인터의 값
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue); // ref값
+
+    setEnteredName('');
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input value={enteredName} ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
