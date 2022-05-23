3a5f831cca1df5af7f334bf9127dabf3917efa0f
heojieun <gjekdmlwl@naver.com>
2022-05-23 17:20:28 +0900

    redux 파일 만들기

---
+++ 09-redux-project/src/store/index.js
@@ -0,0 +1,27 @@
+// redux 로직
+
+// redux 임포트
+// const redux = require('redux');
+import { createStore } from 'redux';
+
+// 함수 만들기
+const counterReducer = (state = { counter: 0 }, action) => {
+  if(action.type === 'increment') {
+    return {
+      counter: state.counter + 1
+    }
+  }
+
+  if(action.type === 'decrement') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+
+  return state;
+}
+
+// 스토어 만들기
+const store = createStore(counterReducer);
+
+export default store;
