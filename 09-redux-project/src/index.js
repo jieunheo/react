4c12cff5592e6bceba81f84017814fc52a3ccf69
heojieun <gjekdmlwl@naver.com>
2022-05-23 17:25:43 +0900

    스토어 제공하기

--- 09-redux-project/src/index.js
+++ 09-redux-project/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; // 제공자 컴포넌트
 
 import './index.css';
 import App from './App';
+import store from './store/index'; // 제공할 스토어
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
