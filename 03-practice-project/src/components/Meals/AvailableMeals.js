f5d3183f640cef4c7e7228f839219fa0cc867637
heojieun <gjekdmlwl@naver.com>
2022-05-23 11:31:49 +0900

    더미 데이터를 fetch를 이용하여 외부에서 가져온 값으로 대체

--- 03-practice-project/src/components/Meals/AvailableMeals.js
+++ 03-practice-project/src/components/Meals/AvailableMeals.js
@@ -1,38 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const dummyList = DUMMY_MEALS.map(meal =>
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    // useEffect 안에는 프로미스를 바로 넣을 수 없음
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-study-2cfc9-default-rtdb.firebaseio.com/meals.json')
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+      for(const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          discription: responseData[key].description,
+          price: responseData[key].price
+        });
+      };
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map(meal =>
     <MealItem
       id={meal.id}
       key={meal.id}
@@ -46,7 +43,7 @@ const AvailableMeals = () => {
     <section className={classes.meals}>
       <Card>
         <ul>
-          {dummyList}
+          {mealsList}
         </ul>
       </Card>
     </section>
