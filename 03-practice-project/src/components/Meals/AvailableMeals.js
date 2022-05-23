1e3c067166e3ab8bfe90fe360ef76b5bb8c6a39c
heojieun <gjekdmlwl@naver.com>
2022-05-23 12:52:55 +0900

    오류처리를 위한 state 사용

--- 03-practice-project/src/components/Meals/AvailableMeals.js
+++ 03-practice-project/src/components/Meals/AvailableMeals.js
@@ -5,7 +5,9 @@ import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
-  const [meals, setMeals] = useState([]);
+  const [meals, setMeals] = useState([]); // 음식 리스트
+  const [isLoading, setIsLoading] = useState(true); // 로딩 여부
+  const [httpError, setHttpError] = useState(); // 에러 여부
 
   useEffect(() => {
     // 병합 충돌 있었음 20220523
@@ -13,6 +15,11 @@ const AvailableMeals = () => {
     const fetchMeals = async () => {
       // 외부 데이터 가져오기
       const response = await fetch('https://react-study-2cfc9-default-rtdb.firebaseio.com/meals.json')
+
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       // 해당 데이터를 json 형식으로 만들기 
       const responseData = await response.json();
 
@@ -29,10 +36,32 @@ const AvailableMeals = () => {
 
       // state에 값 넣기
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    // fetchMeals(); --> promose는 try/catch문이 안먹힘
+    //               --> .catch() 안에 error시 코드 입력
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if(isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if(httpError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map(meal =>
     <MealItem
