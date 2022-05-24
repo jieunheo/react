055659a3235ad8ba01fc8509d2d50d1d264ea275
heojieun <gjekdmlwl@naver.com>
2022-05-24 10:00:23 +0900

    dispatch를 이용하여 counterReducer 가져오기

--- 09-redux-project/src/components/Counter.js
+++ 09-redux-project/src/components/Counter.js
@@ -1,12 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // redux store에 대한 action을 보냄
+  const dispatch = useDispatch();
+
   // redux가 관리하는 counter
   // useSelector --> react-redux가 리덕스 스토어에 서브스크립션을 설정
   //                 -> 컴포넌트 업데이트
   const counter = useSelector(state => state.counter);
+
+  // dispatch를 이용하여 counterReducer 가져오기
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -14,6 +26,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
