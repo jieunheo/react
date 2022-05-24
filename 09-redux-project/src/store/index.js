30f84db177c3bdc166b3cde8d458880df5f918f6
heojieun <gjekdmlwl@naver.com>
2022-05-24 12:49:54 +0900

    createSlice를 사용하여 state와 dispatch 관리하기

--- 09-redux-project/src/store/index.js
+++ 09-redux-project/src/store/index.js
@@ -3,9 +3,31 @@
 // redux 임포트
 // const redux = require('redux');
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit'; // createReducer도 유사
 
 // 초기 값
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: 'counter', // 상태 식별자
+  initialState, //: initialState, 초기값
+  reducers: {
+    // redux toolkit의 기능으로, state 값을 직접 수정하는 것 같은 코드를 작성할 수 있음
+    //                          -> 실제 state를 직접 바꾸는 것이 아님
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
 
 // 함수 만들기
 const counterReducer = (state = initialState, action) => {
