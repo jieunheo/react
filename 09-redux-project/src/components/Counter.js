56094caa06aa920f5401a9f2c4ccd5f57f6930ca
heojieun <gjekdmlwl@naver.com>
2022-05-24 09:35:13 +0900

    useSelector를 이용하여 redux를 적용

--- 09-redux-project/src/components/Counter.js
+++ 09-redux-project/src/components/Counter.js
@@ -1,12 +1,19 @@
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  // redux가 관리하는 counter
+  // useSelector --> react-redux가 리덕스 스토어에 서브스크립션을 설정
+  //                 -> 컴포넌트 업데이트
+  const counter = useSelector(state => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
