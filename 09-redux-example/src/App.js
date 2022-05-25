b25d2d95d95ba2f1b755343969ebaeb0f48b889e
heojieun <gjekdmlwl@naver.com>
2022-05-25 16:25:31 +0900

    상태 적용

--- 09-redux-example/src/App.js
+++ 09-redux-example/src/App.js
@@ -1,27 +1,71 @@
-import { findByPlaceholderText } from '@testing-library/react';
-import { useEffect } from 'react';
-import { useSelector  } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch  } from 'react-redux';
 
+import { uiActions } from './store/ui-slice';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
 
 function App() {
+  const dispatch = useDispatch();
+
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    fetch('https://react-study-2cfc9-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
-    });
-  }, [cart]);
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!'
+      }));
+
+      // useEffect 자체 에서는 비동기 사용 불가
+      const response = await fetch('https://react-study-2cfc9-default-rtdb.firebaseio.com/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      });
+
+      // error 처리
+      if(!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      // 결과
+      // const responseData = await response.json();
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Send cart data successfully!!'
+      }));
+    };
+
+    sendCartData().catch(error => (
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed.'
+      }))
+    ));
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification &&(
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
