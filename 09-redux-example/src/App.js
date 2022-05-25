adaa22e86d656ad6ccef38446f263f070b0a6f08
heojieun <gjekdmlwl@naver.com>
2022-05-25 15:54:25 +0900

    useEffect를 이용하여 firebase로 cart 데이터가 수정될 때마다 전송하기

--- 09-redux-example/src/App.js
+++ 09-redux-example/src/App.js
@@ -1,3 +1,5 @@
+import { findByPlaceholderText } from '@testing-library/react';
+import { useEffect } from 'react';
 import { useSelector  } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +8,14 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-study-2cfc9-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
 
   return (
     <Layout>
