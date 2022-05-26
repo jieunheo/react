403766747df71045b7eacba2d758d633a861fefd
heojieun <gjekdmlwl@naver.com>
2022-05-26 13:43:24 +0900

    폼 전송 시 화면 이동 useHistory

--- 10-time-to-practice-starting-code/src/pages/NewQuotes.js
+++ 10-time-to-practice-starting-code/src/pages/NewQuotes.js
@@ -1,11 +1,16 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuotes = () => {
+  const history = useHistory();
+
   const addQuoteHandler = quoteData => {
     const author = quoteData.author;
     const text = quoteData.text;
 
-
+    history.push('/quotes'); // 뒤로 버튼을 통해 이전 페이지로 돌아갈 수 있음 -> 페이지 이동
+    // history.replace(); // 뒤로 버튼으로 이전 페이지 못감 -> 새 페이지 추가
   };
 
   return (
