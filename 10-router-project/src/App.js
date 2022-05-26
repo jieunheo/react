519d9c52b37b4f9e6bcde693e9f7332d2812a699
heojieun <gjekdmlwl@naver.com>
2022-05-26 10:40:35 +0900

    router 파일 위치 수정

--- 10-router-project/src/App.js
+++ 10-router-project/src/App.js
@@ -1,17 +1,20 @@
 import { Route } from 'react-router-dom';
 
-import Welcome from './components/pages/Welcome';
-import Products from './components/pages/Products';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
 
 function App() {
   return (
     <div>
-      <Route path='/welcome'>
-        <Welcome />
-      </Route>
-      <Route path='/products'>
-        <Products />
-      </Route>
+      <header></header>
+      <main>
+        <Route path='/welcome'>
+          <Welcome />
+        </Route>
+        <Route path='/products'>
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
