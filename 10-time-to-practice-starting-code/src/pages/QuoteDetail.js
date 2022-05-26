754a9be0f4dd6b3db140257d07a0f2edce60f207
heojieun <gjekdmlwl@naver.com>
2022-05-26 13:34:03 +0900

    더미데이터 넣고 일치하는 값 뿌려주기

--- 10-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ 10-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,15 +1,30 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'hong', text: 'Learning React!' },
+  { id: 'q2', author: 'heo', text: 'Learning Javascript!' }
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+  // 유효성 검사
+  if(!quote) {
+    return (
+      <p>No Quote Found.</p>
+    );
+  }
+
+
   return (
     <Fragment>
-      <h1>Quotes Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       {/* 경로로 '/quotes/:quoteId/comments'도 가능 */}
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
