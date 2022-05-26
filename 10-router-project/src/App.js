227157fd22ce4d9e1b4695180100e9c3d05a21b4
heojieun <gjekdmlwl@naver.com>
2022-05-26 11:31:35 +0900

    Switch를 이용하여 하나의 페이지만 뿌려주기

--- 10-router-project/src/App.js
+++ 10-router-project/src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -10,15 +10,18 @@ function App() {
     <div>
       <MainHeader />
       <main>
-        <Route path='/welcome'>
-          <Welcome />
-        </Route>
-        <Route path='/products'>
-          <Products />
-        </Route>
-        <Route path='/products/:productId'>
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          {/* exact: 경로가 완벽하게 일치하는지 확인 */}
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/products/:productId'>
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
