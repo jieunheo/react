29e8db8df0bb7239a3669fdb8e82fa791a446257
heojieun <gjekdmlwl@naver.com>
2022-05-27 12:16:21 +0900

    firebase 안에 있는 데이터 뿌려주기(없는 경우 및 에러 처리)

--- 10-time-to-practice-starting-code/src/pages/AllQuotes.js
+++ 10-time-to-practice-starting-code/src/pages/AllQuotes.js
@@ -1,13 +1,41 @@
-import QuoteList from "../components/quotes/QuoteList";
+import { useEffect } from "react";
 
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'hong', text: 'Learning React!' },
-  { id: 'q2', author: 'heo', text: 'Learning Javascript!' }
-];
+import QuoteList from "../components/quotes/QuoteList";
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from '../lib/api';
 
 const AllQuotes = () => {
+  const { sendRequest, status, data: loadedQuotes, error } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  // 로딩중인 경우
+  if(status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  // error
+  if(error) {
+    return <p className='centered focused'>{error}</p>
+  }
+
+  // error는 아니지만 값이 없는 경우
+  if(status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return (
+      <NoQuotesFound />
+    );
+  }
+
   return (
-      <QuoteList quotes={DUMMY_QUOTES} />
+      <QuoteList quotes={loadedQuotes} />
   );
 };
 
