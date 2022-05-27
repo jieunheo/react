0a47dd668e08ba44f7f1e1894696adbd541e1134
heojieun <gjekdmlwl@naver.com>
2022-05-27 15:22:43 +0900

    중첩 라우팅 변경사항

--- 10-router-6-2/src/pages/Welcome.js
+++ 10-router-6-2/src/pages/Welcome.js
@@ -1,12 +1,14 @@
-import { Route } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
-      <Route path="/welcome/new-user">
-        <p>Welcome, new user!</p>
-      </Route>
+      <Link to='new-user'>New User</Link>
+      <Routes>
+        {/* App.js 파일에서 /welcome/* 인 경우 */}
+        <Route path="new-user" element={<p>Welcome, new user!</p>} />
+      </Routes>
     </section>
   );
 };
