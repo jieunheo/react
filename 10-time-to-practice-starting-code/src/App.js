664a0f9e590aa98fd54b12df7de29d08f5819ab4
heojieun <gjekdmlwl@naver.com>
2022-05-27 16:13:00 +0900

    최적화를 위한 React.lazy()와 Suspense

--- 10-time-to-practice-starting-code/src/App.js
+++ 10-time-to-practice-starting-code/src/App.js
@@ -1,32 +1,41 @@
-import { Redirect } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
-import AllQuotes from './pages/AllQuotes';
-import NewQuotes from './pages/NewQuotes';
-import NotFound from './pages/NotFound';
-import QuoteDetail from './pages/QuoteDetail';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+// import AllQuotes from './pages/AllQuotes';
+// import NewQuotes from './pages/NewQuotes';
+// import QuoteDetail from './pages/QuoteDetail';
+// import NotFound from './pages/NotFound';
+
+// 가져오는 타이밍을 지연시킴
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+const NewQuotes = React.lazy(() => import('./pages/NewQuotes'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuotes />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense fallback={<div className='centered'><LoadingSpinner /></div>}>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
+          <Route path='/quotes' exact>
+            <AllQuotes />
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail />
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuotes />
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
