cbb16baa10426d346be6129e5b29e946ad97abc5
heojieun <gjekdmlwl@naver.com>
2022-05-27 10:01:08 +0900

    useHistory, useLocation을 이용하여 정렬 버튼 만들기

--- 10-time-to-practice-starting-code/src/components/quotes/QuoteList.js
+++ 10-time-to-practice-starting-code/src/components/quotes/QuoteList.js
@@ -1,13 +1,44 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+// useHistory: URL을 바꿀 수 있게 함
+// useLocation: history 객체에 접속하게 해줌
+//              -> 최근 로드된 페이지와 URL에 대한 정보가 들어있음
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+// sort code
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search); // javacript 기본 함수
+  // queryParams = {sort: 'asc'}
+  const isSortAscending = queryParams.get('sort') === 'asc'; // 해당 키의 값 가져오기
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortAscending);
+
+  const changeSortHandler = () => {
+    history.push('/quotes?sort=' + (isSortAscending ? 'desc' : 'asc'));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortHandler}>Sort {isSortAscending ? 'Descending' : 'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
