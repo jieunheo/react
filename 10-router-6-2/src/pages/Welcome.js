2bc7d310cad386b06671320e0bb82535c65ef95b
heojieun <gjekdmlwl@naver.com>
2022-05-27 15:28:00 +0900

    route를 한곳에 모은 후 Outlet를 이용하여 위치 표시하기

--- 10-router-6-2/src/pages/Welcome.js
+++ 10-router-6-2/src/pages/Welcome.js
@@ -1,14 +1,12 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
       <Link to='new-user'>New User</Link>
-      <Routes>
-        {/* App.js 파일에서 /welcome/* 인 경우 */}
-        <Route path="new-user" element={<p>Welcome, new user!</p>} />
-      </Routes>
+      {/* Outlet: 중첩 함수가 들어갈 자리를 표시 */}
+      <Outlet />
     </section>
   );
 };
