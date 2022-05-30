1e6a1961606658accc9621d1f97c2235af28ca52
heojieun <gjekdmlwl@naver.com>
2022-05-30 13:16:40 +0900

    인증 상태에 따른 페이지 router 설정

--- 11-token-project/src/App.js
+++ 11-token-project/src/App.js
@@ -1,22 +1,32 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const authContext = useContext(AuthContext);
+  
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
+        {!authContext.isLoggedIn && (
+          <Route path='/auth'>
+            <AuthPage />
+          </Route>
+        )}
         <Route path='/profile'>
-          <UserProfile />
+          {authContext.isLoggedIn && (<UserProfile />)}
+          {!authContext.isLoggedIn && (<Redirect to='/' />)}
+        </Route>
+        <Route path='/*'>
+          <Redirect to='/' />
         </Route>
       </Switch>
     </Layout>
