4d8bdf834ef6713304df44c844e8da0118b08314
heojieun <gjekdmlwl@naver.com>
2022-05-30 11:12:06 +0900

    firebase REST API를 이용한 회원가입 구현 및 에러처리

--- 11-token-project/src/components/Auth/AuthForm.js
+++ 11-token-project/src/components/Auth/AuthForm.js
@@ -1,28 +1,79 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // optional: Add validation
+
+    setIsLoading(true);
+    // 로그인모드인지 확인
+    if(isLogin) {
+
+    } else {
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBqKZUYad3ra3mARIvpWbyzs6Uj_Sj48xg',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true
+          }),
+          headers: { // 전달 데이터의 형식 작성
+            'Content-Type': 'application/json'
+          }
+        }
+      ).then(res => {
+        setIsLoading(false);
+        
+        if(res.ok) {
+          // 회원가입 성공시 진행
+        } else {
+          // 회원가입 실패시 진행
+          return res.json().then(data => {
+            // 에러 모달 띄우기
+            const errorMessage = 'Authentication failed!';
+            // 에러값에 따른 메세지 보여주기
+            // if(data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            alert(errorMessage);
+          });
+        }
+      });
+    }
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request...</p>}
           <button
             type='button'
             className={classes.toggle}
