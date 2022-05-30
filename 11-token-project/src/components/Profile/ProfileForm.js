1cc1b48b0032faea7d6e05b64002411bce641ec2
heojieun <gjekdmlwl@naver.com>
2022-05-30 12:52:26 +0900

    비밀번호 변경 기능 구현

--- 11-token-project/src/components/Profile/ProfileForm.js
+++ 11-token-project/src/components/Profile/ProfileForm.js
@@ -1,11 +1,63 @@
+import { useRef, useContext } from 'react';
+
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const authContext = useContext(AuthContext);
+  const passwordInputRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredNewPassword = passwordInputRef.current.value;
+
+    // 유효성 검사 코드 작성 위치
+    if(enteredNewPassword) {
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBqKZUYad3ra3mARIvpWbyzs6Uj_Sj48xg',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            idToken: authContext.token,
+            password: enteredNewPassword,
+            returnSecureToken: false
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      .then(res => {
+        if(res.ok) {
+          // 비밀번호 변경 성공
+
+          return res.json();
+        } else {
+          // 실패
+          return res.json().then(data => {
+            // 에러 모달 띄우기
+            const errorMessage = 'Authentication failed!';
+            // 에러값에 따른 메세지 보여주기
+            // if(data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            
+            // error메세지를 catch문으로 이동
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+    }
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength='7' ref={passwordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
