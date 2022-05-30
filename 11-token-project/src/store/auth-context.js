136dd06463d2e2e55eeddbc5ec6437a51e687f12
heojieun <gjekdmlwl@naver.com>
2022-05-30 13:26:10 +0900

    토큰을 localStorerage를 이용하여 저장(화면 재 랜더링 하더라도 로그인 유지)

--- 11-token-project/src/store/auth-context.js
+++ 11-token-project/src/store/auth-context.js
@@ -8,16 +8,19 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = props => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token; // 빈문자열 = false, 값있음: true
 
   const loginHandler = token => {
     setToken(token);
+    localStorage.setItem('token', token);
   }
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem('token');
   }
   
   const contextValue = {
