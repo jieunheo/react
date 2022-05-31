0c5875dd8d3eb363ced69e407940fa76610961ce
heojieun <gjekdmlwl@naver.com>
2022-05-31 13:45:37 +0900

    new meetup 페이지에 폼 집어넣기

---
+++ 12-next-starting-project/pages/new-meetup/index.js
@@ -0,0 +1,15 @@
+// 경로: /new-meetup
+
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetup() {
+  const addMeetupHandler = enteredMeetupData => {
+    console.log(enteredMeetupData);
+  };
+
+  return (
+    <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  );
+};
+
+export default NewMeetup;
