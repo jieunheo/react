c741d158cd46b5c0ddb98daa683dae2b65609ec5
heojieun <gjekdmlwl@naver.com>
2022-05-31 17:42:34 +0900

    DB 안에 있는 데이터를 기반으로 id 추출하여 static paths  설정하기

--- 12-next-starting-project/pages/[meetupId]/index.js
+++ 12-next-starting-project/pages/[meetupId]/index.js
@@ -1,5 +1,7 @@
 // 경로: /[meetupId]
 
+import { MongoClient } from 'mongodb';
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails(props) {
@@ -15,20 +17,29 @@ function MeetupDetails(props) {
 
 // getStaticProps 사용 시 params으로 넘어오는 값 정의
 export async function getStaticPaths() {
+  // DB 연결
+  const client = await MongoClient.connect('mongodb+srv://root:root@cluster0.nbtcz.mongodb.net/meetups?retryWrites=true&w=majority');
+  // DB 가져오기
+  const db = client.db();
+
+  // collection(테이블) 이름 설정 및 생성/가져오기
+  const meetupsCollection = db.collection('meetups');
+
+  // .find(첫번째 값, 두번째 값)
+  // 첫번째 값: 특정 필드 값 필터 기준
+  // 두번째 값: 포함할 필드 정의
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  // DB 끊기
+  client.close();
+
   return {
     fallback: false, // false: paths에게 모든 지원되는 meetup value를 포함, true: Nextjs가 페이지를 만듦
-    paths: [
-      {
-        params: {
-          meetupId: 'm1'
-        }
-      },
-      {
-        params: {
-          meetupId: 'm2'
-        }
+    paths: meetups.map(meetup => ({
+      params: {
+        meetupId: meetup._id.toString()
       }
-    ]
+    }))
   }
 };
 
