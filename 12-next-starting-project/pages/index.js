d119c05509d4738a337e5e7bb075a2ab13c06f50
heojieun <gjekdmlwl@naver.com>
2022-05-31 15:19:25 +0900

    getServerSideProps를 이용하여 server side Rendering 작업

--- 12-next-starting-project/pages/index.js
+++ 12-next-starting-project/pages/index.js
@@ -23,16 +23,28 @@ function HomePage(props) {
   );
 };
 
-// pages 폴더 안에 있는 컴포넌트에만 사용 가능 - 비동기
-// 해당 함수가 완료된 후에 컴포넌트가 실행됨 -> 데이터를 포함한 화면이 렌더링 됨
-export async function getStaticProps() { // 무조건 서버측 실행
+// 요청이 들어올 때마다 실행 -> 배열 다음 서버에서 실행
+export async function getServerSideProps(context) {
+  const req = context.req; // 요청
+  const res = context.res; // 응답
 
   return {
     props: { // HomePage에서 받는 props가 됨
-      meetups: DUMMY_MEETUPS,
-    },
-    revalidate: 10 // 점진적 정적 생성 -> 요청이 들어올 때 페이지를 다시 생성하기까지 대기하는 시간(초)
+      meetups: DUMMY_MEETUPS
+    }
   };
 };
+
+// pages 폴더 안에 있는 컴포넌트에만 사용 가능 - 비동기
+// 해당 함수가 완료된 후에 컴포넌트가 실행됨 -> 데이터를 포함한 화면이 렌더링 됨
+// export async function getStaticProps() { // 무조건 서버측 실행
+
+//   return {
+//     props: { // HomePage에서 받는 props가 됨
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 10 // 점진적 정적 생성 -> 요청이 들어올 때 페이지를 다시 생성하기까지 대기하는 시간(초)
+//   };
+// };
 
 export default HomePage;
