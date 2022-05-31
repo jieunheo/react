d73a0bd3450937f2cff48502916b9439286c517c
heojieun <gjekdmlwl@naver.com>
2022-05-31 13:15:43 +0900

    메인 페이지에 meetup list 뿌려주기

---
+++ 12-next-starting-project/pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1', // 아이디
+    title: 'A First Meetup', // 모임 이름
+    image: 'https://t1.daumcdn.net/cfile/tistory/996333405A8280FC23', // 모임 이미지
+    address: 'Some address 5, 12345 Some City', // 모임 주소
+    discription: 'This is a first meetup!' // 내용
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://img.lovepik.com/photo/40173/9974.jpg_wh860.jpg',
+    address: 'Some address 8, 67890 Some City',
+    discription: 'This is a Second meetup!'
+  }
+];
+
+function HomePage() {
+  return (
+    <MeetupList meetups={DUMMY_MEETUPS} />
+  );
+};
+
+export default HomePage;
