e5de1db76c978920dcd59a75c3aff44fbbb254a9
heojieun <gjekdmlwl@naver.com>
2022-06-02 15:38:47 +0900

    Transition로 실제 modal animation 효과 주기

--- 13-1-animations-start/src/App.js
+++ 13-1-animations-start/src/App.js
@@ -47,7 +47,18 @@ class App extends Component {
             />
           )}
         </Transition>
-        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300} /* 제거될 타이밍 */
+          mountOnEnter /* in이 참이면 DOM에 wrap 요소 추가 */
+          unmountOnExit /* DOM에서 완전히 제거 */
+        >
+          {state => (
+            // 어떤 것이 렌더링 되어야 하는지 정의
+            <Modal show={state} closed={this.closeModal} />
+          )}
+        </Transition>
+        {this.state.modalIsOpen ? '' : null}
         {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
