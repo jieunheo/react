d5d37a02c0649fd0c8322ea0855afdeb979aaa8c
heojieun <gjekdmlwl@naver.com>
2022-06-02 15:29:23 +0900

    Transition 사용해보기

--- 13-1-animations-start/src/App.js
+++ 13-1-animations-start/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -7,7 +8,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBack: false
   }
 
   showModal = () => {
@@ -22,6 +24,29 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={() => this.setState(prevState => ({showBack: !prevState.showBack}))}>
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBack}
+          timeout={1000} /* 제거될 타이밍 */
+          mountOnEnter /* in이 참이면 DOM에 wrap 요소 추가 */
+          unmountOnExit /* DOM에서 완전히 제거 */
+        >
+          {state => (
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition: 'all 1s ease-out',
+                opacity: state ==='exiting' ? 0 : 1
+              }}
+            />
+          )}
+        </Transition>
         {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
         {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
