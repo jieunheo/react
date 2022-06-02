471f7daa5b7a9c602b31f41a7914816d78384a7e
heojieun <gjekdmlwl@naver.com>
2022-06-02 16:24:01 +0900

    CSSTransition를 이용하여 enter/exit 상태에 따라 동적으로 class 집어넣기

--- 13-1-animations-start/src/components/Modal/Modal.js
+++ 13-1-animations-start/src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import './Modal.css';
 
@@ -10,29 +10,22 @@ const animatingTiming = {
 
 const modal = (props) => {
     return (
-        <Transition
+        <CSSTransition
             in={props.show}
             timeout={animatingTiming} /* 제거될 타이밍 */
             mountOnEnter /* in이 참이면 DOM에 wrap 요소 추가 */
             unmountOnExit /* DOM에서 완전히 제거 */
+            classNames='fade-side'
+            /*
+            'fade-side'-enter-active
+            'fade-side'-exit-active
+            */
         >
-            {state => {
-                const cssClasses = [
-                    'Modal',
-                    state === 'entering'
-                    ? 'ModalOpen'
-                    : state === 'exiting' ? 'ModalClosed' : null
-                ];
-                
-
-                return (
-                    <div className={cssClasses.join(' ')}>
-                        <h1>A Modal</h1>
-                        <button className="Button" onClick={props.closed}>Dismiss</button>
-                    </div>
-                );
-            }}
-        </Transition>
+            <div className='Modal'>
+                <h1>A Modal</h1>
+                <button className="Button" onClick={props.closed}>Dismiss</button>
+            </div>
+        </CSSTransition>
     );
 };
 
