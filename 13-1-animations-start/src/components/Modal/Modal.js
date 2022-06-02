93d39671b0be88be3c663e83b6cc40e080171c06
heojieun <gjekdmlwl@naver.com>
2022-06-02 16:29:09 +0900

    fade-side 대신 객체를 넘겨 원하는 class 이름 사용하기

--- 13-1-animations-start/src/components/Modal/Modal.js
+++ 13-1-animations-start/src/components/Modal/Modal.js
@@ -15,11 +15,13 @@ const modal = (props) => {
             timeout={animatingTiming} /* 제거될 타이밍 */
             mountOnEnter /* in이 참이면 DOM에 wrap 요소 추가 */
             unmountOnExit /* DOM에서 완전히 제거 */
-            classNames='fade-side'
-            /*
-            'fade-side'-enter-active
-            'fade-side'-exit-active
-            */
+            classNames={{
+                enter: '',
+                enterActive: 'ModalOpen',
+                exit: '',
+                exitActive: 'ModalClosed'
+                /* appear/appearActive: 처음으로 DOM에 렌더링 될 때 사용 */
+            }}
         >
             <div className='Modal'>
                 <h1>A Modal</h1>
