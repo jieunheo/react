cbd244334a8144336708cf447eb15ecc9bca9481
heojieun <gjekdmlwl@naver.com>
2022-06-03 15:44:47 +0900

    실제 서버에 요청하지 않고 비슷한 순서를 거쳐 더미 값을 제공하는 과정

--- 15-testing-project/src/components/Async.test.js
+++ 15-testing-project/src/components/Async.test.js
@@ -4,6 +4,15 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('renders posts if request secceeds', async () => {
+    // 실제 서버에 요청하지 않고 비슷한 순서를 거쳐 더미 값을 제공하는 과정
+    window.fetch = jest.fn(); // jest.fn: 더미 함수 만들어주기
+    window.fetch.mockResolvedValueOnce({// 호출 시 결정되어야 할 값을 설정
+      json: async () => [{
+        id: 'p1',
+        title: 'First post'
+      }]
+    });
+
     render(<Async />);
 
     // getAllByRole: 복수의 값을 가져오기
