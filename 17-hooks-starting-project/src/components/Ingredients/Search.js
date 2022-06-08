95e0535e844e75d76ce149c8b7eadcffd84593e4
heojieun <gjekdmlwl@naver.com>
2022-06-08 14:24:10 +0900

    setTimepot을 이용하여 입력이 완료된 후에만 http 호출을 하도록(timer 1개만 사용)

--- 17-hooks-starting-project/src/components/Ingredients/Search.js
+++ 17-hooks-starting-project/src/components/Ingredients/Search.js
@@ -1,38 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(({ onLoadIngredients }) => {
   const [enteredFilter, setEnteredFilter] = useState('');
+  const filterInputRef = useRef();
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hook-update-7f183-default-rtdb.firebaseio.com/ingredients.json' + query)
-    .then(response => {
-      if(!response.ok) {
-        throw new Error('Error');
+    // 시간 지연
+    const timer = setTimeout(() => {
+      // timeout 실행 시점과 현재의 값이 같은 경우
+      if(enteredFilter === filterInputRef.current.value) {
+        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hook-update-7f183-default-rtdb.firebaseio.com/ingredients.json' + query)
+        .then(response => {
+          if(!response.ok) {
+            throw new Error('Error');
+          }
+          return response.json();
+        }).then(data => {
+          const loadedIngredients = [];
+          for(const key in data) {
+            loadedIngredients.push({
+              id: key,
+              title: data[key].title,
+              amount: data[key].amount
+            });
+          }
+          onLoadIngredients(loadedIngredients);
+        }).catch(error => console.log(error));
       }
-      return response.json();
-    }).then(data => {
-      const loadedIngredients = [];
-      for(const key in data) {
-        loadedIngredients.push({
-          id: key,
-          title: data[key].title,
-          amount: data[key].amount
-        });
-      }
-      onLoadIngredients(loadedIngredients);
-    }).catch(error => console.log(error));
-  }, [enteredFilter, onLoadIngredients]);
+    }, 600);
+
+    // clean up 함수
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [enteredFilter, onLoadIngredients, filterInputRef]);
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" onChange={(event) => setEnteredFilter(event.target.value)} />
+          <input
+            type="text"
+            onChange={(event) => setEnteredFilter(event.target.value)}
+            value={enteredFilter}
+            ref={filterInputRef}
+          />
         </div>
       </Card>
     </section>
