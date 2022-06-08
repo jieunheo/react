20d04ff4a22dabc7269e482ee7b92dd54ef87aad
heojieun <gjekdmlwl@naver.com>
2022-06-08 14:46:33 +0900

    오류처리

--- 17-hooks-starting-project/src/components/Ingredients/Ingredients.js
+++ 17-hooks-starting-project/src/components/Ingredients/Ingredients.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setIngredients(filteredIngredients);
@@ -39,17 +41,26 @@ const Ingredients = () => {
   const removeItemHandler = (id) => {
     setIsLoading(true);
 
-    fetch(`https://react-hook-update-7f183-default-rtdb.firebaseio.com/ingredients/${id}.json`, {
+    fetch(`https://react-hook-update-7f183-default-rtdb.firebaseio.com/ingredients/${id}.jsona`, {
       method: 'DELETE'
     }).then(response => {
       setIsLoading(false);
-      
+
       setIngredients(prevState => prevState.filter(item => item.id !== id));
+    }).catch(error => {
+      setError(error.message);
+      setIsLoading(false);
     });
+  };
+
+  const clearError = () => {
+    setError(null);
   };
 
   return (
     <div className="App">
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+
       <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
 
       <section>
