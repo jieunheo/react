12ba991ed8b6ba4a71f1dd10f4b6613a054fbdb8
heojieun <gjekdmlwl@naver.com>
2022-06-08 13:35:55 +0900

    firebase에 데이터 저장/조회 구현

--- 17-hooks-starting-project/src/components/Ingredients/Ingredients.js
+++ 17-hooks-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch('https://react-hook-update-7f183-default-rtdb.firebaseio.com/ingredients.json')
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Error');
+      }
+      return response.json();
+    }).then(data => {
+      const loadedIngredients = [];
+      for(const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        });
+      }
+      setIngredients(loadedIngredients);
+    }).catch(error => console.log(error));
+  }, []);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch('https://react-hook-update-7f183-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      if(!response.ok) {
+        throw new Error('Error');
+      }
+      return response.json();
+    }).then(data => {
+      setIngredients(prevState => [
+        ...prevState,
+        { id: data.name, ...ingredient }
+      ]);
+    }).catch(error => console.log(error));
+  };
+
+  const removeItemHandler = (id) => {
+    setIngredients(prevState => prevState.filter(item => item.id !== id));
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={removeItemHandler} />
       </section>
     </div>
   );
